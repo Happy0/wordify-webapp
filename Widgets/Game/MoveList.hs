4b5424ea3ce368c4b093a910ab33f461787b3189
Gordon Martin <gordon.hugh.martin@gmail.com>
2015-07-28 21:16:20 +0000

    wups

--- Widgets/Game/MoveList.hs
+++ Widgets/Game/MoveList.hs
@@ -1,14 +1,15 @@
 module Widgets.Game.MoveList (MoveSummary(Passed, Exchanged, Scored)) where
 
-	import Wordify.Rules.FormedWord
+    import Wordify.Rules.FormedWord
+    import Import
 
-	data MoveSummary = Passed | Exchanged | Scored FormedWords
+    data MoveSummary = Passed | Exchanged | Scored FormedWords
 
-	createMoveList :: [MoveSummary] -> Widget
-	createMoveList moves =
-		do
+    createMoveList :: [MoveSummary] -> Widget
+    createMoveList moves =
+        do
             [whamlet|
                 <table>
-                	$for move <- moves
-                		<p> I'm a move!~
+                    $forall move <- moves
+                        <p> I'm a move!~
             |]
