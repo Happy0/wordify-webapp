2d266c83002b47b4a27c06fb3fab862c1856a313
Gordon Martin <gordon.hugh.martin@gmail.com>
2015-07-28 22:18:18 +0000

    Wrote some untested code. Going to bed :P.

--- Widgets/Game/MoveList.hs
+++ Widgets/Game/MoveList.hs
@@ -11,5 +11,28 @@ module Widgets.Game.MoveList (MoveSummary(Passed, Exchanged, Scored)) where
             [whamlet|
                 <table>
                     $forall move <- moves
-                        <p> I'm a move!~
+                        <tr>
+                            $case move
+                                $of Scored formed
+                                    <td>^{templateScoreMove formed}
+                                    <td>#{overallScore formed}
+                                $of Passed
+                                    <td>passed
+                                $of Exchanged
+                                    <td>exchanged
             |]
+
+    templateScoreMove :: FormedWords -> Widget
+    templateScoreMove formed =
+        do
+            [whamlet|
+                <table>
+                    $forall word <- allWordsFormed
+                        <tr>
+                            <td>#{prettyPrintIntersections placed word}
+                            <td>#(scoreWord word)
+
+            |]
+        where
+            allWordsFormed = allWords formed
+            placed = playerPlacedMap formed
