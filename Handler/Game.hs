88c2315c4f4c55c45c262782bda1af552b62efc1
Gordon Martin <gordon.hugh.martin@gmail.com>
2015-08-06 08:50:09 +0000

    Started playing around with websockets.

--- Handler/Game.hs
+++ Handler/Game.hs
@@ -1,6 +1,36 @@
+
 module Handler.Game where
 
 import Import
+import Yesod.Core
+import Yesod.WebSockets
+import qualified Data.Text.Lazy as TL
+import qualified Data.Monoid as M
+import Data.Text (Text)
 
 getGameR :: String -> Handler Html
-getGameR string = error "Not yet implemented: getGameRR"
+getGameR string = do
+    webSockets testApp
+    defaultLayout $ do
+        [whamlet|
+            <p> baws
+        |]
+        toWidget
+            [julius|
+                var url = document.URL,
+
+                url = url.replace("http:", "ws:").replace("https:", "wss:");
+                var conn = new WebSocket(url);
+
+                conn.onmessage = function(e) {
+                    console.dir(e);
+                };
+
+            |]
+
+
+testApp :: WebSocketsT Handler ()
+testApp = do
+    sendTextData ("blah blah message 1" :: Text)
+    sendTextData $ (("blah blah message 2") :: Text)
+    redirect HomeR
