9153108723b7e9b5d00df6f092561957dba71c19
Gordon Martin <gordon.hugh.martin@gmail.com>
2015-12-02 19:25:40 +0000

    Stuff, things.

---
+++ ui/round/src/ctrl.js
@@ -0,0 +1,12 @@
+var Scrabbleground = require('scrabbleground');
+
+module.exports = function(opts) {
+
+    // Our scrabbleground controller for manipulating the state of the game
+    var scrabbleGroundCtrl = Scrabbleground.ctrl;
+
+    var moveMade = function(move) {
+
+    }
+
+};
---
+++ ui/round/src/view.js
@@ -0,0 +1,7 @@
+var scrabbleground = require('scrabbleground');
+
+module.exports = function(ctrl) {
+
+    var scrabblegroundView = scrabbleground(ctrl.scrabblegroundCtrl);
+
+}
