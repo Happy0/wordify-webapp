6354cf0a1dccf6196aa5f87dcf046eff350226e2
Gordon Martin <gordon.hugh.martin@gmail.com>
2015-12-12 16:56:39 +0000

    Refactor.

--- Handler/Game.hs
+++ Handler/Game.hs
@@ -124,7 +124,8 @@ setupPrerequisets serverGame =
 gameApp :: TVar ServerGame -> TChan GameMessage -> Maybe Text -> Maybe Int -> WebSocketsT Handler ()
 gameApp game channel maybePlayerId playerNumber =
         race_
-            (forever $ atomically (toJSONResponse . handleChannelMessage <$> readTChan channel) >>= sendTextData)
+            (forever $ 
+                    handleChannelMessage <$> atomically (readTChan channel) >>= sendTextData . toJSONResponse)
             (forever $ 
                 do
                     msg <- receiveData
