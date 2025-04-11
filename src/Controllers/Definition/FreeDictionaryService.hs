cd7812928a9919065ec5c78a218365246477d022
Gordon Martin <gordonhughmartin@gmail.com>
2025-04-11 12:20:33 +0100

    wip

--- src/Controllers/Definition/FreeDictionaryService.hs
+++ src/Controllers/Definition/FreeDictionaryService.hs
@@ -4,26 +4,34 @@ module Controllers.Definition.FreeDictionaryService (getDefinitions) where
     import Data.Aeson
     import Data.Aeson.Types
     import Controllers.Definition.DefinitionService
-    import ClassyPrelude (IO, Either, undefined) 
-
+    import ClassyPrelude (IO, Either, undefined, Maybe, (<$>), pure, mapM, ($), toList) 
+    
     data FreeDictionaryService = FreeDictionaryService {urlPrefix :: T.Text}
 
     data FreeDictionaryDefinition = FreeDictionaryDefinition {definition :: T.Text, synonyms :: [T.Text]}
     data FreeDictionaryMeaning = FreeDictionaryMeaning {partOfSpeech :: T.Text, definitions :: [FreeDictionaryDefinition]}
-    data FreeDictionaryResponseItem = FreeDictionaryResponseItem {meanings :: [FreeDictionaryMeaning]}
+    data FreeDictionaryResponseItem = FreeDictionaryResponseItem {word :: T.Text, meanings :: [FreeDictionaryMeaning]}
     data FreeDictionaryResponse = FreeDictionaryResponse [FreeDictionaryResponseItem]
 
     instance FromJSON FreeDictionaryDefinition where
         parseJSON (Object response) = undefined
 
     instance FromJSON FreeDictionaryMeaning where
-        parseJSON (Object response) = undefined
+        parseJSON = withObject "Meaning" $ \obj -> do
+            partOfSpeech <- obj .: "partOfSpeech"
+            definitions <- obj .: "definitions"
+            pure $ FreeDictionaryMeaning partOfSpeech definitions
 
     instance FromJSON FreeDictionaryResponseItem where
-        parseJSON (Object response) = undefined
+        parseJSON = withObject "WordMeanings" $ \obj -> do
+            word <- obj .: "word"
+            meanings <- obj .: "meanings"
+            pure $ FreeDictionaryResponseItem word meanings
 
     instance FromJSON FreeDictionaryResponse where
-        parseJSON (Object response) = undefined
+        parseJSON = withArray "ResponseItems" $ \arr -> do 
+            items <- mapM parseJSON (toList arr)
+            pure $ FreeDictionaryResponse items
 
     instance DefinitionService FreeDictionaryService where
         getDefinitions service word = undefined
