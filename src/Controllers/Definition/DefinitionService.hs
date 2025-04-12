3537d6fbede4c211d79500376b61be80f02e6534
Gordon Martin <gordonhughmartin@gmail.com>
2025-04-12 14:11:22 +0100

    wip

--- src/Controllers/Definition/DefinitionService.hs
+++ src/Controllers/Definition/DefinitionService.hs
@@ -1,6 +1,8 @@
 module Controllers.Definition.DefinitionService (DefinitionService(getDefinitions), Definition(Definition), DefinitionServiceImpl(DefinitionServiceImpl), definitions, toDefinitionServiceImpl) where
     import qualified Data.Text as T
-    import ClassyPrelude (IO, Either, Show, Maybe)
+    import ClassyPrelude (IO, Either, Show, Maybe, void, Int, pure, (>>), (>>=), Either(Left, Right), ($), (*), join, (<$>), either, id, (.))
+    import Control.Concurrent.Async (race)
+    import Control.Concurrent (forkIO, threadDelay)
 
     data Definition = Definition {partOfSpeech :: T.Text, definition :: T.Text, example :: Maybe T.Text} deriving Show
 
@@ -13,3 +15,16 @@ module Controllers.Definition.DefinitionService (DefinitionService(getDefinition
 
     toDefinitionServiceImpl :: DefinitionService a => a -> DefinitionServiceImpl
     toDefinitionServiceImpl service = DefinitionServiceImpl (getDefinitions service)
+
+    withDefinitionsAsync :: DefinitionServiceImpl -> T.Text -> Int -> (Either T.Text [Definition] -> IO ()) -> IO ()
+    withDefinitionsAsync definitionService word timeoutSeconds withDefinitionsAction = void $ forkIO $
+        do 
+            definitionOrError <- getDefinitionOrTimeout definitionService word timeoutSeconds
+            withDefinitionsAction definitionOrError
+
+    getDefinitionOrTimeout :: DefinitionServiceImpl -> T.Text -> Int -> IO (Either T.Text [Definition])
+    getDefinitionOrTimeout (DefinitionServiceImpl getDefinitions) word timeoutAfter = do 
+        race (timeout timeoutAfter) (getDefinitions word) >>= pure . either id id
+        where 
+            timeout :: Int -> IO (Either T.Text [Definition])
+            timeout timeoutAfter =  threadDelay (1000000 * timeoutAfter) >> pure (Left "Timed out while getting definition.")
