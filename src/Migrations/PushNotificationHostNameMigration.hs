bc632ddb545aa7bd35423e96189fb51638a2aabc
Gordon Martin <gordonhughmartin@gmail.com>
2026-02-12 23:23:23 +0000

    eeesh

--- src/Migrations/PushNotificationHostNameMigration.hs
+++ src/Migrations/PushNotificationHostNameMigration.hs
@@ -5,16 +5,27 @@ module Migrations.PushNotificationHostNameMigration (runPushNotificationHostName
     import Database.Persist.Sql
     import qualified Model as M
     import Database.Persist (Entity(..))
+    import Control.Exception (catch, SomeException)
 
     runPushNotificationHostNameMigration :: ConnectionPool -> Maybe T.Text -> IO ()
     runPushNotificationHostNameMigration pool maybeAppRoot = do
         putStrLn "Running push notification host name migration..."
+        ensureBaseHostNameColumn pool
         subscriptions <- withPool pool $ selectList [] [] :: IO [Entity M.PushNotificationSubscription]
         mapM_ (migrateSubscription pool baseHostName) subscriptions
       where
         baseHostName = case maybeAppRoot of
             Just root -> root
             Nothing -> "http://127.0.0.1:4000"
+
+    ensureBaseHostNameColumn :: ConnectionPool -> IO ()
+    ensureBaseHostNameColumn pool =
+        withPool pool (rawExecute
+            "ALTER TABLE push_notification_subscription ADD COLUMN base_host_name VARCHAR NOT NULL DEFAULT 'http://127.0.0.1:4000'"
+            []) `catch` handleAlreadyExists
+      where
+        handleAlreadyExists :: SomeException -> IO ()
+        handleAlreadyExists _ = putStrLn "base_host_name column already exists, skipping."
 
     migrateSubscription :: ConnectionPool -> T.Text -> Entity M.PushNotificationSubscription -> IO ()
     migrateSubscription pool baseHostName (Entity key subscription) =
